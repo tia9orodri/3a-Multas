b1ea3b944700db0f4bb0717310cfce5efd44e3d5
Tiago Rodrigues <aluno19058@ipt.pt>
2019-04-30 16:40:36 +0100

    Adicionar anotadores para formatar o comportamento do modelo Agentes

--- BaseDados/BaseDados/Models/Agentes.cs
+++ BaseDados/BaseDados/Models/Agentes.cs
@@ -10,10 +10,22 @@ namespace BaseDados.Models {
         [Key] //identifica este atributo como Primary Key
         public int ID { get; set; }
 
+        [Required(ErrorMessage ="O nome é de preenchimento obrigatório.")]
+        [StringLength(50,ErrorMessage ="O {0} deve ter, no máximo, {1} caracteres.")]
+        [RegularExpression("[A-ZÁÉÍÓÚ][a-záéíóúàèÌòùãõîôûâç]+(( | e | de | do | das | da | dos |-|')[A-ZÁÉÍÓÚ][a-zzáéíóúàèÌòùãõîôûâç]+)*",
+            ErrorMessage ="O {0} só pode conter letras. Cada palavra deve começar com Maiúscula.")]
+
+        //[A-ZÁÉÍÓÚ][a-záéíóúàèÌòù]+(( | e | de | do | das | da | dos |-|')[A-ZÁÉÍÓÚ][a-z]+)* 
+        //( ((e|de|do|das|da|dos) )?)
+        //( ((e|d(a(s)?|o(s)?|e)) )?)
         public string Nome { get; set; }
 
+        [Required(ErrorMessage = "A esquadra é de preenchimento obrigatório.")]
+        [StringLength(20, ErrorMessage = "A {0} deve ter, no máximo, {1} caracteres.")]
+        [RegularExpression("[A-Z][a-z]+(( |-)[A-Z][a-z]+)*",
+            ErrorMessage = "O {0} só pode conter letras. Cada palavra deve começar com Maiúscula.")]
         public string Esquadra { get; set; }
-
+        
         public string Fotografia { get; set; }
 
         //*************************************
