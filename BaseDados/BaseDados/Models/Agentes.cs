b34ea3d7f0c23fc76ad3661439bccb8415929da0
Tiago Rodrigues <aluno19058@ipt.pt>
2019-04-30 15:20:38 +0100

    teste

--- BaseDados/BaseDados/Models/Agentes.cs
+++ BaseDados/BaseDados/Models/Agentes.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
 namespace BaseDados.Models {
     public class Agentes {
 
+        [Key] //identifica este atributo como Primary Key
         public int ID { get; set; }
 
         public string Nome { get; set; }
---
+++ BaseDados/BaseDados/Models/keyAttribute.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace BaseDados.Models
+{
+    internal class keyAttribute : Attribute
+    {
+    }
+}
